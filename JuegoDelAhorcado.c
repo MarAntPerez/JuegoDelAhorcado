da7ceb3971ef47aae40cd6a707f3a5968deb6e93
Marco <maphy1707@gmail.com>
2022-01-20 15:05:14 -0600

    Juego del ahorcado con un arreglo de diez indices.

--- JuegoDelAhorcado.c
+++ JuegoDelAhorcado.c
@@ -3,50 +3,81 @@
 
 int main(int argc, char *argv[]) {
  	
-	char palabra[4];
+ 	/*Esta variable guarda la palabra en un arreglo*/
+	char palabra[10];
 	
-	palabra[0] = 'A';
+	/*Este es el indice de la palabra guardada*/
+	palabra[0] = 'M';
 	palabra[1] = 'U';
-	palabra[2] = 'T';
-	palabra[3] = 'O';
+	palabra[2] = 'R';
+	palabra[3] = 'C';
+	palabra[4] = 'I';
+	palabra[5] = 'E';
+	palabra[6] = 'L';
+	palabra[7] = 'A';
+	palabra[8] = 'G';
+	palabra[9] = 'O';
 	
-	char palabraCopia[4];
+	/*Esta variable guarda un arreglo para guardar los caracteres que ingrese el usuario*/
+	char palabraCopia[10];
 	
+	/*Este es el indice de los caracteres que ingresara el usuario*/
 	palabraCopia[0] = '_';
 	palabraCopia[1] = '_';
 	palabraCopia[2] = '_';
 	palabraCopia[3] = '_';
+	palabraCopia[4] = '_';
+	palabraCopia[5] = '_';
+	palabraCopia[6] = '_';
+	palabraCopia[7] = '_';
+	palabraCopia[8] = '_';
+	palabraCopia[9] = '_';
 
+	/*Esta variable guarda el caracter ingresado por el usuario*/
 	char caracterIngresado;
 	
-	int contadorVidas = 3;
+	/*Esta variable guarda las vidas del usuario*/
+	int contadorVidas = 5;
 
+	/*Esta constante le dice al programa si el usuario encontro alguna letra del arreglo*/
 	const int LETRA_ENCONTRADA = 0;
+	
+	/*Esta constante le dice al programa que el usurio no encontro una letra del arreglo*/
 	const int LETRA_NO_ENCONTRADA = 1;
+	
+	/*Esta constante cambia el vaos de los indices del arreglo cunado el usurio encuentra una letra*/
 	const int ARROBA = '@';
+	
+	/*Esta contante le dice al programa que el usuario ha encontrado todas las letras y por consecuencia ha ganado*/
 	const int JUEGO_GANADO = 0;
+	
+	/*Esta constante le dice al programa que ek usurio no ha encontrado ninguna letra y por ende ha perdido*/
 	const int TODAS_LAS_VIDAS_PERDIDAS = 0;
 	
 	int bandera;
 	
-	printf("\nLa palabra guardada en memoria tiene cuatro letras.");
-	printf("\n\n\n ---- Tendras 3 vidas, si fallas alguna letra se te iran restando. ---");
+	printf("\nLa palabra guardada en memoria tiene diez letras.");
+	printf("\n\n\n ---- Tendras 5 vidas, si fallas alguna letra se te iran restando. ---");
+	/*Esta bandera le dice al programa que el usuario ha ganado*/
 	int banderaJuegoGanado = -1;
 
 	while(banderaJuegoGanado != JUEGO_GANADO && contadorVidas != TODAS_LAS_VIDAS_PERDIDAS){
 		printf("\n");
+		/*Esta variable itera la palabra con un contador en tres for*/
 		int contador;
-	    for(contador=0; contador<4; contador++){
+	    for(contador=0; contador<11; contador++){
 	    	char letra = palabraCopia[contador];
 	    	printf("%c ", letra);
 	    }
 	
+		/*Esta variable le da el valor de una letra no encontrada a una bandera para despues cambiar su valor a una letra encontrada y guardarla en el segundo arreglo*/
 		int banderaLetraEncontrada = LETRA_NO_ENCONTRADA;
 		printf("\n\nIngresa una letra(solo mayusculas).");
 		scanf(" %c", &caracterIngresado);
 		system("cls");
 		
-		for(contador=0; contador<4; contador++){
+		for(contador=0; contador<11; contador++){
+			/*Esta variable guarda las letras de la iteracion de la palabra*/
 			char letra = palabra[contador]; 
 			if(caracterIngresado==letra){
 				banderaLetraEncontrada = LETRA_ENCONTRADA;
@@ -56,6 +87,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		
+		
 		if(banderaLetraEncontrada == LETRA_ENCONTRADA){
 			printf("\nAcertaste la letra %c.", caracterIngresado);	
 		}else if(banderaLetraEncontrada == LETRA_NO_ENCONTRADA){
@@ -63,8 +95,10 @@ int main(int argc, char *argv[]) {
 		    contadorVidas--;
 		    printf("\nPerdiste una vida, te restan %i", contadorVidas);
 		}
+		/*Esta variable gurda las letras encontradas*/
  		int contadorLetrasEncontradas = 0;
-		for(contador=0; contador<4; contador++){
+		for(contador=0; contador<11; contador++){
+			/*Esta variable guarda las letras de la iteracion de la palabra y luego las guarda en el segundo arreglo*/
 			char letra = palabra[contador]; 
 			if(ARROBA==letra){
 				contadorLetrasEncontradas++;
@@ -72,18 +106,17 @@ int main(int argc, char *argv[]) {
 		}
 		
 		
-		if(contadorLetrasEncontradas == 4){
+		if(contadorLetrasEncontradas == 10){
 			banderaJuegoGanado = JUEGO_GANADO;
 			 printf("\n ============= Felicidades ganaste =============");
 			 printf("\n La palabra era: ");
-			 	for(contador=0; contador<4; contador++){
+			 	for(contador=0; contador<11; contador++){
+			 		/*Esta variable guarda las letras de la iteracion de la palabra y las manda al segundo arreglo*/
 					char letra = palabraCopia[contador];
 					printf("%c", letra); 
 					
 			}
 		}
-		
-	
 
 	}
 	
